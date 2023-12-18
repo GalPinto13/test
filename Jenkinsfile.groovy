b52bd68d78628351fe71669411b1bb86a57576ce
GalPinto13 <154219249+GalPinto13@users.noreply.github.com>
2023-12-18 12:31:09 +0200

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 // Checkout your Git repository
                 script {
-                    git branch: 'main', credentialsId: 'GP13Git', url: 'https://github.com/GalPinto13/test'
+                    git branch: 'main', url: 'https://github.com/GalPinto13/test'
                 }
             }
         }
